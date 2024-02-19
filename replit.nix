26147f9d3abb9cf7cc2ed67011fb0ac9b30570cb
sandeshlavshetty <138968398+sandeshlavshetty@users.noreply.github.com>
2024-02-19 08:27:15 +0000

    after selenium and extensions

--- replit.nix
+++ replit.nix
@@ -1,5 +1,7 @@
 { pkgs }: {
     deps = [
+      pkgs.ungoogled-chromium
+      pkgs.chromedriver
       pkgs.cowsay
     ];
 }
--- scraper.py
+++ scraper.py
@@ -1,10 +1,21 @@
 import requests
-
+from bs4 import BeautifulSoup
 job_site_url='https://www.timesjobs.com/candidate/job-search.html?searchType=Home_Search&from=submit&asKey=OFF&txtKeywords=&cboPresFuncArea=35'
 
+#does not execute javascript
 response = requests.get(job_site_url)
 
 print('Status Code:', response.status_code)
 
 with open('trending.html', 'w') as f:
     f.write(response.text)
+
+doc = BeautifulSoup(response.text,'html.parser')
+
+print('Page title:',doc.title.text)
+
+#find all job li tags 
+job_li = doc.find_all('li',class_='clearfix job-bx wht-shd-bx')
+
+print(f'found {len(job_li)} jobs')
+
